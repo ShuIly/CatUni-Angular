fcea01454c6759d12ccfda52f8978e611cc42a22
ShuIly <hacerqwett@gmail.com>
2018-03-11 13:24:20 +0200

    Modified menu. Optimized navigation links.

--- src/app/common/menu/menu.component.ts
+++ src/app/common/menu/menu.component.ts
@@ -1,28 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css', './menu.responsive.component.css'],
 })
-export class MenuComponent {
+export class MenuComponent implements AfterViewInit {
+  @ViewChild('navigation') nav;
+  @ViewChild('navList') navList;
+  @ViewChild('navSublist') navSublist;
+  @ViewChild('homeIcon') homeIcon;
+  @ViewChild('homeLink') homeLink;
+
+  ngAfterViewInit() {
+    this.nav = document.querySelector('nav');
+    this.navList = document.querySelector('.nav-list');
+    this.navSublist = document.querySelector('.nav-sublist');
+    this.homeIcon = document.querySelector('.home-icon');
+    this.homeLink = document.getElementById('home-link');
+  }
+
   toggleMenu(): void {
-    document.querySelector('nav').classList.toggle('nav-stretch');
-    document.querySelector('.nav-list').classList.toggle('switch-mobile');
+    this.nav.classList.toggle('nav-stretch');
+    this.navList.classList.toggle('switch-mobile');
 
     // On menu close, nav sub-list should close too.
-    document.querySelector('.nav-sublist').classList.remove('show-sublist');
-
-    // On menu close, should remove black background from home link.
-    document.getElementById('home-link').classList.remove('mobile-link-hovered');
+    this.navSublist.classList.remove('show-sublist');
 
     // On menu close, should switch back rotating icon.
-    document.querySelector('.home-icon').classList.remove('mobile-icon-rotate');
+    this.homeIcon.classList.remove('mobile-icon-rotate');
+
+    // On menu close, should remove background from home link.
+    this.homeLink.classList.remove('mobile-link-hovered');
   }
 
   toggleSubMenu(): void {
-    document.getElementById('home-link').classList.toggle('mobile-link-hovered');
-    document.querySelector('.nav-sublist').classList.toggle('show-sublist');
-    document.querySelector('.home-icon').classList.toggle('mobile-icon-rotate');
+    this.homeLink.classList.toggle('mobile-link-hovered');
+    this.navSublist.classList.toggle('show-sublist');
+    this.homeIcon.classList.toggle('mobile-icon-rotate');
+  }
+
+  closeMenu(): void {
+    this.nav.classList.remove('nav-stretch');
+    this.navList.classList.remove('switch-mobile');
+
+    this.navSublist.classList.remove('show-sublist');
+    this.homeIcon.classList.remove('mobile-icon-rotate');
+
+    // When changing routes, should go to top of page.
+    scroll(0, 0);
   }
 }
