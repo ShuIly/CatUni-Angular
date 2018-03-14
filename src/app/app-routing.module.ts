0b31f631ff578eeeb628e304f21cd13917189dd8
ShuIly <hacerqwett@gmail.com>
2018-03-14 22:48:56 +0200

    Added 404 page (needs refactoring). Slight css improvements.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,23 @@ import { QuizModule } from './components/quiz/quiz.module';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { NotFoundComponent } from './components/404-not-found/not-found.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
-  { path: 'quiz', component: QuizComponent }
+  { path: 'quiz', component: QuizComponent },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ]
 
 @NgModule({
   declarations: [
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
---
+++ src/app/components/404-not-found/not-found.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css', './not-found-responsive.component.css'],
+})
+export class NotFoundComponent {}
